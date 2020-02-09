9434f2452f5e26a218f8d90def6bfa10a3b9c671
Chaitrali Deshmukh <42701774+ChaiDeshmukh@users.noreply.github.com>
2020-02-08 16:26:36 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,24 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+
+1) Design HashMap
+Time Complexity : Constant O(N)
+Space Complexity : 
+Did this code successfully run on Leetcode : Yes
+
+2) Design MinStack
+
+Time Complexity : Constant O(1)
+Space Complexity : 
+Did this code successfully run on Leetcode : Yes
